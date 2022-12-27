ab91e2229c6b3d6dcf41eb264ef91c6825b5734c
seckot <97128735+seckot@users.noreply.github.com>
2022-12-28 00:38:19 +0300

    Add files via upload

--- DE_Sprint_Project/src/data_mart.py
+++ DE_Sprint_Project/src/data_mart.py
@@ -12,3 +12,14 @@ print(df_category_name)
 #Общее количество новостей из всех источников по данной категории за все время
 df_category_name_all_news = len(df_intermediate_layer_news[df_intermediate_layer_news['category']=='Мир'])
 print(df_category_name_all_news)
+
+#Количество новостей данной категории для каждого из источников за все время
+#lenta.ru
+df_category__all_news_lenta = len(df_intermediate_layer_news[(df_intermediate_layer_news['category']=='Мир') & (df_intermediate_layer_news['link'].str.contains('lenta.ru', regex= True, na=False))])
+print(df_category__all_news_lenta)
+#www.vedomosti.ru
+df_category__all_news_vedomosti = len(df_intermediate_layer_news[(df_intermediate_layer_news['category']=='Мир') & (df_intermediate_layer_news['link'].str.contains('vedomosti.ru', regex= True, na=False))])
+print(df_category__all_news_vedomosti)
+#tass.ru
+df_category__all_news_tass = len(df_intermediate_layer_news[(df_intermediate_layer_news['category']=='Мир') & (df_intermediate_layer_news['link'].str.contains('tass.ru', regex= True, na=False))])
+print(df_category__all_news_tass)
