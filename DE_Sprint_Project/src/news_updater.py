2c9d3c436ed7c8c2f406da80ec7b9bb18b8faeb1
seckot <97128735+seckot@users.noreply.github.com>
2022-12-28 21:05:42 +0300

    Add files via upload

--- DE_Sprint_Project/src/news_updater.py
+++ DE_Sprint_Project/src/news_updater.py
@@ -16,14 +16,17 @@ for url in rss_feeds:
 df_download_update_news = pd.DataFrame(posts, columns=["title", "link", "published", "category"])
 
 df_upload_raw_news = pd.read_csv(r'C:\Sprint\raw_news_data.csv', on_bad_lines='skip')
+df_intermediate_layer_news_old = pd.read_csv(r'C:\Sprint\news_intermediate_layer.csv', on_bad_lines='skip')
 
-df_worker = pd.concat([df_upload_raw_news,df_download_update_news], sort=False, axis=0)
-df_intermediate_layer_news = df_worker.drop_duplicates()
-df_intermediate_layer_news['published'] = pd.to_datetime(df_intermediate_layer_news['published']).dt.date
+df_intermediate_layer_news = pd.concat([df_upload_raw_news,df_intermediate_layer_news_old,df_download_update_news], sort=False, axis=0)
+
+
+df_intermediate_layer_news['published'] = pd.to_datetime(df_intermediate_layer_news['published']).dt.normalize()
 df_intermediate_layer_news["category"] = df_intermediate_layer_news["category"].replace(['Международная панорама'], 'Мир')
 df_intermediate_layer_news["category"] = df_intermediate_layer_news["category"].replace(['Экономика и бизнес'], 'Экономика')
 df_intermediate_layer_news["category"] = df_intermediate_layer_news["category"].replace(['Моя страна '], 'Россия')
 df_intermediate_layer_news["category"] = df_intermediate_layer_news["category"].replace(['Армия и ОПК '], 'Силовые структуры')
 df_intermediate_layer_news["category"] = df_intermediate_layer_news["category"].replace(['Наука и техника'], 'Технологии')
 df_intermediate_layer_news["category"] = df_intermediate_layer_news["category"].replace(['Авто'], 'Транспорт')
-df_intermediate_layer_news.to_csv(r'C:\Sprint\news_intermediate_layer.csv', encoding='utf-8', index=False)
+df_intermediate_layer_news_to_file = df_intermediate_layer_news.drop_duplicates()
+df_intermediate_layer_news_to_file.to_csv(r'C:\Sprint\news_intermediate_layer.csv', encoding='utf-8', index=False)
